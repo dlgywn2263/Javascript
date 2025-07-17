db30372f8953680732b0522c7bd6d7d03c04ef80
dlgywn2263 <lhj-3988@naver.com>
2025-07-17 16:59:44 +0900

    Add files via upload

--- Day03/condition/quiz1.js
+++ Day03/condition/quiz1.js
@@ -1,3 +1,4 @@
+
 const degree = +window.prompt("각도 입력");
 if (degree > 0 && degree < 90) {
   window.console.log("예각");
@@ -20,13 +21,11 @@ const has_under = guk_score <= 60 || math_scroe <= 60 || eng_score <= 60
 
 if (average >= 90) {
   window.console.log("A");
-} else if (average >= 80 && average < 90) {
+} else if (average >= 80) {
   window.console.log("B");
-} else (average < 80) {
+} else {
   window.console.log("C");
 } 
-
-
 if(has100){
     window.console.log("Good")
 }
@@ -37,6 +36,30 @@ if(has_under){
 const drink = +window.prompt("음료 종류 수");
 const amount = +window.prompt("구매할 음료 수");
 const money = +window.prompt("투입한 금액 (원)");
+
 if(drink == 1){
-    const americano_price = 3000 * amount 
+    const total_cost = 3000 * amount 
+    if (money >= total_cost){
+        window.console.log(`아메리카노 ${amount}개 , 잔돈 ${money - total_cost}원`);
+    } else{
+        window.console.log(`잔액이 부족합니다`);
+    }
+}
+else if(drink == 2){
+    const total_cost1 = 4000 * amount 
+    if (money >= total_cost1){
+        window.console.log(`레몬에이드 ${amount}개 , 잔돈 ${money - total_cost1}원`);
+    }else{
+        window.console.log(`잔액이 부족합니다`);
+    }
+}else if(drink == 3){
+    const total_cost2 = 3500 * amount
+    if(money >= total_cost2){
+        window.console.log(`카페라떼 ${amount}개 , 잔돈 ${money - total_cost2}원`)
+    }else{
+        window.console.log(`잔액이 부족합니다`);
+    }
+}
+else {
+    window.console.log("dd")
 }
